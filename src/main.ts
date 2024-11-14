9845b80b6e36de78807fa877d89dddf979b9b1cd
Joel_Pauth <168111753+Joel-Pauth@users.noreply.github.com>
2024-11-13 20:03:06 -0600

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,52 @@
-
-const getData = async () => {
-    const data = await fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+interface Pokemon {
+    id: number;
+    name: string;
+    sprites: {
+      front_default: string;
+    };
+  }
   
-    return data;
+  interface PokemonAbility {
+    ability: {
+      name: string;
+    };
+  }
+  
+  interface PokemonType {
+    type: {
+      name: string;
+    };
+  }
+  
+  const fetchPokemon = async (id: number): Promise<Pokemon> => {
+    const response = await fetch(https://pokeapi.co/api/v2/pokemon/${id});
+    return response.json();
   };
   
-  const data = getData();
-  console.log(data);
+  const fetchPokemonAbilities = async (id: number): Promise<PokemonAbility[]> => {
+    const response = await fetch(https://pokeapi.co/api/v2/pokemon/${id});
+    const data = await response.json();
+    return data.abilities;
+  };
   
+  const fetchPokemonTypes = async (id: number): Promise<PokemonType[]> => {
+    const response = await fetch(https://pokeapi.co/api/v2/pokemon/${id});
+    const data = await response.json();
+    return data.types;
+  };
+  
+  const main = async () => {
+    const pokemonId =5;
+  
+    const pokemon = await fetchPokemon(pokemonId);
+    console.log(Pokémon: ${pokemon.name}, ID: ${pokemon.id}, Image: ${pokemon.sprites.front_default});
+  
+    const abilities = await fetchPokemonAbilities(pokemonId);
+    console.log(Habilidades: ${abilities.map(a => a.ability.name).join(', ')});
+  
+
+    const types = await fetchPokemonTypes(pokemonId);
+    console.log(Tipos: ${types.map(t => t.type.name).join(', ')});
+  };
+  
+  main().catch(console.error);
